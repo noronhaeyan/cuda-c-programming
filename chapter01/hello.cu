995173307842db46e14cf4619147245c6c55a1c3
noronhaeyan <noronhaeyan@gmail.com>
2025-07-19 17:42:45 +0000

    chp 2

--- chapter01/hello.cu
+++ chapter01/hello.cu
@@ -3,12 +3,15 @@
 
 /*
  * A simple introduction to programming in CUDA. This program prints "Hello
- * World from GPU! from 10 CUDA threads running on the GPU.
+ * World from GPU! from 100 CUDA threads running on the GPU.
  */
 
 __global__ void helloFromGPU()
 {
-    printf("Hello World from GPU!\n");
+    if (threadIdx.x == 5)
+    {
+        printf("Hello World from GPU thread %i!\n", threadIdx.x);
+    }
 }
 
 int main(int argc, char **argv)
